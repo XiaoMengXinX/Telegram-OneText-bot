7fbe75361c0a22f77efc2a594f0855fad1dd0781
XiaoMengXinX <xibaole2333@gmail.com>
2022-08-04 18:52:03 +0800

    custom sentence support

--- bot/api/bot.go
+++ bot/api/bot.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	onetext "github.com/XiaoMengXinX/OneTextAPI-Go"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -50,6 +51,51 @@ func BotHandler(w http.ResponseWriter, r *http.Request) {
 			Name:  "onetext.png",
 			Bytes: img,
 		})
+		msg.ReplyToMessageID = update.Message.MessageID
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+	if update.Message.Command() == "custom" {
+		/*
+			text:xxxx
+			from:xxxx
+			by:xxxx
+			record_time:xxxx
+			create_time:xxxx
+		*/
+		var s onetext.Sentence
+		msgText := update.Message.CommandArguments()
+		args := strings.Split(msgText, "\n")
+		for _, arg := range args {
+			if strings.HasPrefix(arg, "text:") {
+				s.Text = strings.TrimPrefix(arg, "text:")
+			}
+			if strings.HasPrefix(arg, "from:") {
+				s.From = strings.TrimPrefix(arg, "from:")
+			}
+			if strings.HasPrefix(arg, "by:") {
+				s.By = strings.TrimPrefix(arg, "by:")
+			}
+			if strings.HasPrefix(arg, "record_time:") {
+				s.Time = []string{strings.TrimPrefix(arg, "record_time:")}
+			}
+			if strings.HasPrefix(arg, "create_time:") && len(s.Time) > 0 {
+				s.Time = append(s.Time, strings.TrimPrefix(arg, "create_time:"))
+			}
+		}
+		img, err := utils.CreateOnetextImage(s)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		msg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileBytes{
+			Name:  "onetext.png",
+			Bytes: img,
+		})
+		msg.ReplyToMessageID = update.Message.MessageID
 		_, err = bot.Send(msg)
 		if err != nil {
 			log.Println(err)
